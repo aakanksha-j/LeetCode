453f0bca49e2c1832b69c524af7cd4035e990d20
Aakanksha <aaks2492@gmail.com>
2020-09-12 08:47:41 -0700

    Solution to 27 Remove element when elements are rare and modified two pointer approach to include main.

--- 27. Remove Element/approach_2_when_elements_are_rare.py
+++ 27. Remove Element/approach_2_when_elements_are_rare.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeElement(self, nums, val):
-        k=0
-        for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1
-        return k
+        i=0
+        n=len(nums)
+        while i<n:
+            if nums[i]==val:
+                nums[i]=nums[n-1] #swap
+                n-=1 # reduce array size by one
+            else:
+                i+=1
+        return n
 
 def main():
     numbers = [0,1,2,2,3,0,4,2]
