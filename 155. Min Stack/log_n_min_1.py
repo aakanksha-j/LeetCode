096dcb2e3ba47f07f97aacc8ac4df43f2163b7f4
Aakanksha <aaks2492@gmail.com>
2020-11-25 22:32:52 -0800

    Solution to Min Stack using min inbuilt function.

---
+++ 155. Min Stack/log_n_min_1.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.items = []
+
+    def push(self, x: int) -> None:
+        self.items.append(x)
+
+    def pop(self) -> None:
+        if len(self.items) != 0:
+            self.items.pop()
+        return None
+
+    def top(self) -> int:
+        return self.items[len(self.items)-1]
+
+    def getMin(self) -> int:
+        if len(self.items) != 0:
+            return min(self.items)
+        else:
+            return None
+
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
