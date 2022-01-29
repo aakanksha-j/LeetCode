db41d1fec0417c0dd3f5c4f800c12cfd6f97259e
Aakanksha <aaks2492@gmail.com>
2022-01-28 21:42:27 -0800

    Make cycle, find new tail and head
    Similar to own approach. Take care of 3 base cases, 1. list is empty,
    2. list has one element, 3. k = 0. Iterate over the entire linked list
    to get the length. Start from head to iterate over the linked list again
    till we get the tail. Assign new head and tail.
    Since we start from the head, formula used for 'for' loop is n - (k % n)
    - 1. This approach does not change the value of k. It starts from the
    head. It uses for loop to iterate instead of while so has smaller code.

---
+++ 61. Rotate List/leetcode_solution_2.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # base cases
+        if not head or not head.next or k == 0:
+            return head
+
+        # close the linked list into a ring
+        old_tail = head
+        n = 1
+        while old_tail.next:
+            old_tail = old_tail.next
+            n += 1
+        old_tail.next = head
+
+        # find new tail: (n - (k % n) - 1)th node
+        # find new head: (n - (k % n))th node
+        new_tail = head
+        for i in range(n - (k%n) -1):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+
+        # break the ring
+        new_tail.next = None
+
+        return new_head
