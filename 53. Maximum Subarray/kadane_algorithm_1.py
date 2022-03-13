1c85de6024d71cc2c589acc2ec274f0e66e17695
Aakanksha <aaks2492@gmail.com>
2022-03-13 10:49:45 -0700

    Using Kadane's kadane_algorithm
    Use two variables maxsum and cursum. Maxsum gets updated whenever cursum
    gets a greater value. Start maxsum with nums[0]. Use for loop to go
    through all elements in array instead of while loop. Cursum maintains
    sum of all elements. For each run of loop, we check whether cursum is
    less than 0. We can ignore the previous sum and start afresh as they
    will not contribute positively. Add the current element and update
    maxsum. Return maxsum.
    time: O(n) as we go through each element in list exactly once.
    space: O(1) as we use only 2 constant variables maxsum and cursum.

---
+++ 53. Maximum Subarray/kadane_algorithm_1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:\
+        # neetcode solution
+
+        maxsum = nums[0]
+        cursum = 0
+
+        for n in nums:
+            if cursum < 0:
+                cursum = 0
+            cursum += n
+            maxsum = max(maxsum, cursum)
+
+        return maxsum
