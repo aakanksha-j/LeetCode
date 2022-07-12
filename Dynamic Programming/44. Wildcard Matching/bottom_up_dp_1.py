9121cd5fbf4f2757b4077d1ae6f8a86d571841fc
Aakanksha <aaks2492@gmail.com>
2022-07-12 16:06:35 -0700

    Bottom up DP
    both wildcard and regex will have space and time complexity of O(S.P)
    where s is string and p is pattern ~ O(n^2)

---
+++ Dynamic Programming/44. Wildcard Matching/bottom_up_dp_1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
+        dp[0][0] = True
+
+        for j in range(1, len(p) + 1):
+            if p[j - 1] != '*':
+                break
+            dp[0][j] = True
+
+        #print(dp)
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] in (s[i - 1], '?'):
+                    dp[i][j] = dp[i - 1][j - 1]
+                if p[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+
+        #print(dp)
+
+        return dp[-1][-1]
+
+# https://leetcode.com/problems/wildcard-matching/discuss/256025/Python-DP-with-illustration
