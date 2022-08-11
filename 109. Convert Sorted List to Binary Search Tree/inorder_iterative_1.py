1be1f3b190604e75e7648587976d302a0102e1d7
Aakanksha <aaks2492@gmail.com>
2022-08-11 11:18:11 -0700

    109 Convert Sorted List to Binary Search Tree
    8 11 2022
    
    Inorder traversal in iterative way
    We pass start, end, status alongwith root node in stack as arguments.
    Status is used because we are doing inorder traversal, we will pass
    root node again to stack but with flag status as True. When the flag is
    True, assign the value of linked list's current node and move pointer of
    linked list to the next node. Add left and right nodes to the stack only
    when the values of start <= mid - 1 for left and mid + 1 <= end. This is
    with right side elements only so for [-10,-3,0,5,9] linked list, bst
    created will be [0,-10,5,null,-3,null,9] with traversal as following:
    0 4 False
    0 1 False
    0 1 True
    1 1 False
    1 1 True
    0 4 True
    3 4 False
    3 4 True
    4 4 False
    4 4 True
    
    Time is O(N) as we visit each node in linked list atleast once and space
    for stack is O(N)

---
+++ 109. Convert Sorted List to Binary Search Tree/inorder_iterative_1.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # iterative inorder solution - https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/discuss/1244022/Python-O(N)-Iterative-with-stack-Need-NOT-to-convert-to-Array
+
+    # time: O(N) - visit every node
+    # space: O(N) - stack
+
+    def find_len(self, head):
+        curr = head
+        n = 0
+        while curr:
+            n += 1
+            curr = curr.next
+        return n
+
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        if not head: return None
+
+        n = self.find_len(head)
+
+        curr = head
+        root = TreeNode(-1)
+        stack = [(0, n-1, root, False)]
+
+        while stack:
+            start, end, node, status = stack.pop()
+
+            mid = (start + end) // 2
+
+            if status is False:
+                if mid + 1 <= end:
+                    node.right = TreeNode(-1)
+                    stack.append((mid+1, end, node.right, False))
+                stack.append((start, end, node, True))
+                if start <= mid - 1:
+                    node.left = TreeNode(-1)
+                    stack.append((start, mid-1, node.left, False))
+
+            else:
+                node.val = curr.val
+                curr = curr.next
+
+        return root
+        
