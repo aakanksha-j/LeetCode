a3d5f8f85387c6ddd99d3e48669a9c9027021f2b
Aakanksha <aaks2492@gmail.com>
2022-09-28 13:29:46 -0700

    9 28 2022 Maximum subarray revision

--- Greedy/53. Maximum Subarray/kadane_algorithm_1.py
+++ Greedy/53. Maximum Subarray/kadane_algorithm_1.py
@@ -12,3 +12,25 @@ class Solution:
             maxsum = max(maxsum, cursum)
 
         return maxsum
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:  
+        # own approach
+        max_sum = nums[0]
+        cur_sum = 0
+        
+        for n in nums:
+            cur_sum = max(cur_sum + n, n)
+            max_sum = max(max_sum, cur_sum)
+        
+        return max_sum
+         
+            
+        
+TestCase
+1. [2,3,-2,4]
+2. [-2,0,-1]
+3. [-2,1,-3,4,-1,2,1,-5,4]
+4. [5,4,-1,7,8]
+5. [1]
