1b34fd618a04126e92d1c41df778a715e53a2bc5
Aakanksha <aaks2492@gmail.com>
2023-04-13 13:16:27 -0700

    2023 04 13 using split method

--- 71. Simplify Path/two_while_stack_1.py
+++ 71. Simplify Path/two_while_stack_1.py
@@ -45,3 +45,20 @@ class Solution:
         print(stack)
 
         return '/' + '/'.join(stack)
+
+# another approach:
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        print(stack)
+        for ch in path.split('/'):
+            if ch == '' or ch == '.':
+                continue
+            elif ch == '..':
+                if stack:
+                    stack.pop()
+            else:
+                 stack.append(ch)
+        return '/'+'/'.join(stack)
+    
