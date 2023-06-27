2977c470918cab365a130f46e190f0c6cf34b14e
Aakanksha <aaks2492@gmail.com>
2023-06-27 14:56:01 -0700

    2023 06 27 binary search and bisect to solve 35 search insert position

---
+++ 35. Search Insert Position/binary_search_1.py
@@ -0,0 +1,54 @@
+"""
+35. Search Insert Position
+Easy
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+"""
+import bisect
+from typing import List
+
+# time O(logN)
+# space O(1)
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n - 1
+        while left <= right:
+            # print(left, right)
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+            # print(mid, left, right)
+            # print()
+        return left
+    
+        # return bisect.bisect_left(nums, target)
