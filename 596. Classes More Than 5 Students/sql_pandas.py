a8ce700242590b88f669b49857ebf09b5cf6386c
Aakanksha <aaks2492@gmail.com>
2023-08-21 16:43:54 -0700

    SQL and pandas solution to Data Aggregation sum

---
+++ 596. Classes More Than 5 Students/sql_pandas.py
@@ -0,0 +1,68 @@
+"""
+596. Classes More Than 5 Students
+Easy
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it."""
+
+# Write your MySQL query statement below
+# SELECT class
+# FROM Courses
+# GROUP BY class
+# HAVING COUNT(*) >= 5
+
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby('class').size().reset_index(name = 'count')
+    df = df[df['count'] >= 5]
+    return df[['class']]
+
+# 'groupby' method groups the dataframe by the column 'class'
+# 'size' method counts the number of occurences for each unique class
+# 'reset_index' method renames this count of students column as 'count'
